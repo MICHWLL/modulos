b1a44b5c5ec6995e8d96b036192e69790a1f75d1
MICHWLL <135914336+MICHWLL@users.noreply.github.com>
2025-01-10 12:40:13 -0500

    saludar.py

--- saludar.py
+++ saludar.py
@@ -3,3 +3,17 @@ def saludar():
 
 def saludarNombre(nombre):
     print(f"hola,{nombre}!")
+
+
+# recorrer un iterable sin usar while o for , hacemos uso de las funciones iter () y next()
+
+frutas=("pera")
+
+frutas = iter(frutas)
+print(next(frutas))
+print(next(frutas))
+print(next(frutas))
+print(next(frutas))
+
+
+#si el arreglo en la posicion 4 esta vacia, manda un error 
