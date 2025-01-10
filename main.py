f78c767ffb26c4568a45c3614eda28853eeda765
MICHWLL <135914336+MICHWLL@users.noreply.github.com>
2025-01-10 12:56:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#from  saludar import saludarNombre as holi # con la palabra as se renombra a las funciones 
+import saludar
+
+print(f"hola como estas ? {saludar.saludarNombre('anny')}");
+
+
+      
